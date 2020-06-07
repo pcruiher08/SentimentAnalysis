134feb0b612910384cc8c639cf3ae3d945444ae2
Sebastian Martínez <55563384+vSebas@users.noreply.github.com>
2020-06-07 15:34:26 -0500

    Youtube comment scraper

---
+++ Youtube_scraper.py
@@ -0,0 +1,91 @@
+'''
+Victor Sebastian Martinez
+Youtube comment scraper
+
+Usage: python <search_term> <number_of_comments>
+
+For preprocessing:
+https://towardsdatascience.com/nlp-for-beginners-cleaning-preprocessing-text-data-ae8e306bef0f
+
+Issues:
+If there are non-english comments, the desired number of comments could not be met
+the isEnglish functions sometimes classifies english sentences as non-english
+
+'''
+# -*- coding: utf-8 -*-
+import os
+import time
+import sys
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer 
+from selenium import webdriver
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+def isEnglish(sentence):
+    try:
+        sentence.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+driver = webdriver.Firefox()
+
+# Create directories
+to_search = sys.argv[1]
+cwd = os.getcwd()
+absolute_test_dir = os.path.join(cwd, "test/")
+
+try:
+    os.makedirs(absolute_test_dir, exist_ok=True)
+except OSError:
+        print(f"Creation of {absolute_test_dir} directory failed")
+
+# Search for a video
+driver.get("https://www.youtube.com/results?search_query=" + to_search)
+Video = driver.find_element_by_xpath("//div[@class='text-wrapper style-scope ytd-video-renderer']")
+Video.click()
+time.sleep(3)
+
+# Search for comments
+desired_comments = int(sys.argv[2])
+comments = []
+# comment_section = driver.find_element_by_xpath('//*[@id="comments"]')
+MAX_COMMENTS = driver.find_element_by_xpath('//*[@class="count-text style-scope ytd-comments-header-renderer"]')
+MAX_COMMENTS = int(MAX_COMMENTS.text.split()[0].replace(",", ""))
+
+# Scroll down until desired number of comments are reached
+desired_comments = MAX_COMMENTS if desired_comments > MAX_COMMENTS else desired_comments
+while desired_comments > len(comments):
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+    time.sleep(2)
+    comments = driver.find_elements_by_xpath('//*[@id="content-text"]')
+
+# Preprocess and save
+stop_words = stopwords.words('english')
+tokenizer = RegexpTokenizer(r"\w+(?:[-']\w+)*|'|[-.(]+|\S\w*") # \w - Any character, + - match one or more
+lemmatizer = WordNetLemmatizer()
+with open(absolute_test_dir + to_search + '.txt', 'w') as f:
+    # while collected_comments < desired_comments and collected_comments < len(comments):
+    for comment in range(desired_comments):
+        comments[comment] = remove_emoji(comments[comment].text)
+        if isEnglish(comments[comment]):
+            word_tokens = tokenizer.tokenize(comments[comment])
+            filtered_sentence = [w for w in word_tokens if not w in stop_words]
+            lem_text = [lemmatizer.lemmatize(i).lower() for i in filtered_sentence]
+            # stem_text = [stemmer.stem(i) for i in filtered_sentence]
+            f.write(' '.join(lem_text) + '\n')
+
+driver.quit()
