00b244132e59da24979e39b28ff327d30038b1e6
Mildred Gil <mi.mitel@hotmail.com>
2020-06-16 01:57:54 -0500

    arreglalo porfis

--- Method.py
+++ Method.py
@@ -1,4 +1,5 @@
 from twitter import Tweets
+from TestModels import runModels
 
 def call(method):
     list_terms = getTerms()
@@ -10,13 +11,14 @@ def call(method):
 
     methodCall = switch.get(method)
     methodCall(list_terms)
+    runModels("Comments/" + method + ".txt")
 
 def getTerms():
     x = input ("insert terms:")
     return x.split(' ')
 
 def initTwitter(list_terms):
-    print(list_terms)
+     """ Init Twitter API with specified terms. """
     t = Tweets(list_terms)
 
 def initYoutube(list_terms):
